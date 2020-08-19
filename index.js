b0cd624ecafe6f26ea11d58ca737be6ad2bd9b83
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-19 19:43:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+const map = function(sourceArray, callback) {
+    let mapped = []
+    sourceArray.forEach((element) => {
+        mapped.push(callback(element))
+    })
+    return mapped
+}
+
+const reduce = function(sourceArray, reducer, init = sourceArray[0]) {
+    let total = init
+    if (total != sourceArray[0]) {
+        sourceArray.forEach((element) => {
+            total = reducer(element, total)
+        })
+    } else {
+        for (let i = 1; i < sourceArray.length; i++) {
+            total = reducer(sourceArray[i], total)
+        }
+    }
+    return total
+}
